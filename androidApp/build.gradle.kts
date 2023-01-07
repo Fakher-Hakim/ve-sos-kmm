69f16935bb27bfc1564515fe57e7a48c70a3b05d
Fakher Hakim <fakher.hakim@bridge-softwares.com>
2023-01-07 18:09:11 +0100

    Bump version code.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.bridge.softwares.vesos"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
     buildFeatures {
